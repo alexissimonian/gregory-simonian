450d0687f66c5bbe95342583a30fdd2a9f2e7fdc
alexis.simonian <alexis.simonian@outlook.com>
2025-02-10 13:59:07 +0000

    take changes to other desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+    import { auth } from "$lib/firebase/firebase.app"
+    let provider = new GoogleAuthProvider();
 	let { children } = $props();
 </script>
 
