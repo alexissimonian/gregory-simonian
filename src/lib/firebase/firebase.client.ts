8a0b99cc31eac8695b818b7adebb61bba24aecc2
alexis <alexis.simonian@outlook.com>
2025-02-13 07:13:58 +0100

    wip firebase admin

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -1,16 +1,23 @@
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp, type FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
-let firebaseClientApp = initializeApp(firebaseConfig);
-let firebaseClientAuth = getAuth(firebaseClientApp);
+let firebaseClientApp: FirebaseApp | undefined;
+
+if (!getApps().length) {
+    firebaseClientApp = initializeApp(firebaseConfig, "client");
+} else {
+    firebaseClientApp = getApps()[0];
+}
+
+const firebaseClientAuth = getAuth(firebaseClientApp);
 
 export { firebaseClientApp, firebaseClientAuth };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,23 @@
 <script>
-    import {GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+    import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
     import { firebaseClientAuth } from "$lib/firebase/firebase.client";
+    import { goto } from "$app/navigation";
 
     async function loginWithGoogle() {
         const provider = new GoogleAuthProvider();
-        const result = await signInWithPopup(firebaseClientAuth, provider);
-        console.log(result);
-    };
+        let result = await signInWithPopup(firebaseClientAuth, provider)
+            .catch((error) => {
+                console.error(error.message);
+            });
+
+        const response = await fetch('/api/auth', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(result)});
+        if (response.ok){
+           goto('/');
+        } else {
+            console.error("Unauthorized");
+        }
+    }
 </script>
 
 <button on:click={loginWithGoogle}>Login with Google</button>
+
