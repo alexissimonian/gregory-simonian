454564c437917ed654a0810fe082719c3dc26dba
alexis.simonian <alexis.simonian@outlook.com>
2025-02-18 09:45:20 +0000

    authorization done

--- src/lib/firebase/firebase.admin.ts
+++ src/lib/firebase/firebase.admin.ts
@@ -1,7 +1,12 @@
 import admin from 'firebase-admin';
+import firebaseServiceAccountCredentials from '../../../firebaseserviceaccount.json';
+
+const serviceAccount = firebaseServiceAccountCredentials as admin.ServiceAccount;
 
 if (!admin.apps.length){
-    admin.initializeApp();
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
 }
 
 export const adminAuth = admin.auth();
--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,11 +1,14 @@
 import { json } from "@sveltejs/kit";
 import { adminAuth } from "$lib/firebase/firebase.admin";
+import { AUTHORIZED_EMAILS } from "$env/static/private";
 
 export async function POST({request}){
     const idToken = await request.json();
     adminAuth.verifyIdToken(idToken).then((decodedToken) => {
-        console.log("here");
-        console.log(decodedToken);
+        let emails = AUTHORIZED_EMAILS.split(',');
+        if (decodedToken.email && emails.includes(decodedToken.email)){
+            return json({ message: 'authorized' }, {status: 200});
+        }
     })
-    return json({status: 200});
+    return json({ message: 'Forbidden' }, { status: 403 });
 }
